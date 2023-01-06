0bcab8eb8f415f313996efa5a4d1a3b046e747d4
Luka Dusak <lux.9833@gmail.com>
2023-01-06 16:35:21 +0100

    15: react router - 02 blog project

---
+++ 15 - react router/02-practice-starting-code/src/pages/QuoteDetails.js
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetails = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(params.quoteId);
+  }, [params.quoteId, sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <div>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+
+      {/* <Route exact path={`/quotes/${params.quoteId}`}> */}
+      <Route exact path={`${match.path}`}>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      {/* <Route path={`/quotes/${params.quoteId}/comments`}> */}
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </div>
+  );
+};
+
+export default QuoteDetails;
